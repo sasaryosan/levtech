2a64b9c4adb0f14009c8ada274cf9ec88c530402
sasaryosan <nexusnoopy20@gmail.com>
2024-09-08 22:01:02 +0900

    9_2commit(定例面談後)

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,14 +7,14 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
-        <a href="">{{ $post->category->name }}</a>
         <h1 class="title">
             {{ $post->title }}
         </h1>
         <div class="content">
             <div class="content__post">
                 <h3>本文</h3>
-                <p>{{ $post->body }}</p>    
+                <p>{{ $post->body }}</p>
+                <a href="">{{ $post->category->name }}</a>
             </div>
         </div>
         <div class="edit">
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\CategoryController; 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,3 +21,4 @@ Route::post('/posts', [PostController::class, 'store']);
 Route::get('/posts/{post}', [PostController::class ,'show']);
 // '/posts/{対象データのID}'にGetリクエストが来たら、PostControllerのshowメソッドを実行する
 Route::delete('/posts/{post}', [PostController::class,'delete']);
+Route::get('/categories/{category}', [CategoryController::class,'index']);
